4e44b6cbd656700464f7d7e4ca17d530f098db10
cmart63 <carlosmartinho63@gmail.com>
2024-03-18 14:39:30 -0300

    Endepoint para retornar os posts de um usuário

--- src/main/java/com/martinho/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/martinho/workshopmongodb/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.martinho.workshopmongodb.resources;
 
+import com.martinho.workshopmongodb.domain.Post;
 import com.martinho.workshopmongodb.domain.User;
 import com.martinho.workshopmongodb.dto.UserDTO;
 import com.martinho.workshopmongodb.services.UserService;
@@ -58,6 +59,13 @@ public class UserResource {
         obj = service.update(obj);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 
 }
